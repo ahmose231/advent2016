e5c931e7bdfae218d5b580bf6adb2f415da7b3ee
ahmose231 <36313066+ahmose231@users.noreply.github.com>
2022-03-15 17:06:06 +0300

    Create 15.c

---
+++ 15.c
@@ -0,0 +1,58 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+#define LEN 128
+#define DISCMAX 6
+
+void main()
+{
+    struct proto_disc {
+        int total;
+        int position;
+    } disc[DISCMAX];
+    int k=0;
+    int n;
+    char line[LEN];
+    while(fgets(line,LEN,stdin)!=NULL)
+    {
+        if(strlen(line)<2) continue;
+        sscanf(line, "Disc #%d has %d positions; at time=0, it is at position %d.",
+                &n, &disc[k].total, &disc[k].position);
+        k++;
+    }
+    
+    
+    int diff;
+    diff=disc[0].total-disc[0].position-1;
+    
+    for(int i=0;i<k;i++)
+        disc[i].position += diff;
+    
+    int clock=diff;
+    int count;
+    int answer;
+    while(1)
+    {
+        count=0;
+        for(int i=0;i<k;i++)
+            if((disc[i].position+i+1)%disc[i].total==0)
+                count++;
+        
+        if(count==k)
+            break;
+            
+        clock+=disc[0].total;
+        
+        for(int i=0;i<k;i++)
+            disc[i].position+=disc[0].total;
+    }
+        
+    printf("%d\n",clock);
+    
+    return;
+}
+
+    
+        
